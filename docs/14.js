68b698f1009845b95d952500586987beec293c19
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-11 13:10:33 +0000

    Deploy TimPietrusky/dotJS2018-WebUSB-for-Digital-Artists to github.com/TimPietrusky/dotJS2018-WebUSB-for-Digital-Artists.git:master

--- docs/14.js
+++ docs/14.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{423:function(n,w,o){"use strict";o.r(w),w.default={}}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{421:function(n,w,o){"use strict";o.r(w),w.default={}}}]);
 //# sourceMappingURL=14.js.map
